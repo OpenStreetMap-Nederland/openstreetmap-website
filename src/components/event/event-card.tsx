9a44acc8a21087805d9087914109657e8c3adbcf
Mart <tjuro@proton.me>
2023-11-24 10:29:53 +0100

    responsive events

--- src/components/event/event-card.tsx
+++ src/components/event/event-card.tsx
@@ -10,8 +10,8 @@ type Props = {
 export function EventCard({ event }: Props) {
   return (
     <Link href={`/events/${event.toHash()}`} key={event.name}>
-      <Card className="p-4 grid grid-cols-3">
-        <div className="flex flex-col col-span-2">
+      <Card className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="flex flex-col col-span-1 lg:col-span-2">
           <h1 className="text-xl font-bold tracking-tight mb-4">
             {event.name}
           </h1>
